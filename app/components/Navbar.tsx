87a59a06fba487509ac63dfb5b9f3541d4c41a81
eeherngSMU <eeherng.tan.2023@scis.smu.edu.sg>
2024-08-31 12:32:30 +0800

    create navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Input } from "@/components/ui/input"
+
+
+
+const Navbar = () => {
+  return (
+    <nav className='border-b flex items-center justify-between px-2'>
+        <a href="/"><img src="playtogether.jpg" className='w-40 h-20 object-cover'/></a>
+        <Input type="text" placeholder='Find your favourite sport' className='max-w-sm' />
+        
+        <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+    </nav>
+  )
+}
+
+export default Navbar
